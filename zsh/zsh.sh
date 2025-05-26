61e50e8f326f09405b5c0c0c5fe45ae00544bde9
tom <32962031+toomuat@users.noreply.github.com>
2025-05-26 15:27:32 +0900

    fix shellcheck error

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -37,8 +37,8 @@ if [ ~/.zshrc -nt ~/.zshrc.zwc ]; then
 fi
 
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
-[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
-[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+[ ! -d "${ZINIT_HOME}" ] && mkdir -p "$(dirname "${ZINIT_HOME}")"
+[ ! -d "${ZINIT_HOME}"/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}"
 source "${ZINIT_HOME}/zinit.zsh"
 
 # プラグインのロード（遅延ロード）
