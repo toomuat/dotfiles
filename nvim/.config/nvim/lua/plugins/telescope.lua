e2bd68a8c50f687904f0b30ca7390f34e035f880
tom <32962031+toomuat@users.noreply.github.com>
2025-06-29 11:53:39 +0900

    fix(nvim): Fix telescope.nvim lazy loading issue
    - Remove early require() calls that prevented plugin installation
    - Move telescope.builtin require() inside key mapping functions
    - Move telescope.actions require() inside opts function
    - Fixed lazy.nvim plugin loading by avoiding early dependency resolution
    
    Fixes telescope plugin not being installed due to premature require() calls
    during lazy.nvim plugin evaluation phase.

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -12,130 +12,124 @@ return {
 			"nvim-telescope/telescope-frecency.nvim",
 			"kkharji/sqlite.lua",
 		},
-		keys = (function()
-			local builtin = require("telescope.builtin")
-			return {
-				{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "ファイル検索" },
-				{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep検索" },
-				{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "バッファ一覧" },
-				{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "ヘルプタグ検索" },
-				{ "<leader>sf", "<cmd>Telescope file_browser<cr>", desc = "ファイルブラウザ" },
-				-- Custom keymaps
-				{
-					";a",
-					function()
-						builtin.oldfiles({ initial_mode = "normal" })
-					end,
-					desc = "最近使ったファイル",
-				},
-				{
-					";b",
-					function()
-						builtin.current_buffer_fuzzy_find({ theme = "dropdown", sorting_strategy = "ascending" })
-					end,
-					desc = "現在バッファ内ファジー検索",
-				},
-				{
-					";f",
-					function()
-						builtin.find_files({ no_ignore = false, hidden = true, initial_mode = "normal" })
-					end,
-					desc = "ファイル検索(隠し含む)",
-				},
-				{
-					";r",
-					function()
-						builtin.live_grep()
-					end,
-					desc = "Grep検索",
-				},
-				{
-					"\\",
-					function()
-						builtin.buffers({ initial_mode = "normal" })
-					end,
-					desc = "バッファ一覧",
-				},
-				{
-					";t",
-					function()
-						builtin.help_tags({ initial_mode = "normal" })
-					end,
-					desc = "ヘルプタグ検索",
-				},
-				{
-					";;",
-					function()
-						builtin.resume({ initial_mode = "normal" })
-					end,
-					desc = "前回の検索を再開",
-				},
-				{
-					";e",
-					function()
-						builtin.diagnostics({ initial_mode = "normal" })
-					end,
-					desc = "診断一覧",
-				},
-				{
-					";c",
-					function()
-						builtin.git_commits({ initial_mode = "normal" })
-					end,
-					desc = "Gitコミット履歴",
-				},
-				{
-					";d",
-					function()
-						builtin.git_status({ initial_mode = "normal" })
-					end,
-					desc = "Gitステータス",
-				},
-				{
-					";h",
-					function()
-						builtin.command_history({ initial_mode = "normal" })
-					end,
-					desc = "コマンド履歴",
-				},
-				{
-					";g",
-					function()
-						require("telescope").extensions.frecency.frecency({ initial_mode = "normal" })
-					end,
-					desc = "最近使ったファイル(頻度順)",
-				},
-				{
-					"sf",
-					function()
-						local telescope = require("telescope")
-						local function telescope_buffer_dir()
-							return vim.fn.expand("%:p:h")
-						end
-						telescope.extensions.file_browser.file_browser({
-							path = "%:p:h",
-							cwd = telescope_buffer_dir(),
-							respect_gitignore = false,
-							hidden = true,
-							grouped = true,
-							previewer = false,
-							initial_mode = "normal",
-							layout_config = { height = 40 },
-						})
-					end,
-					desc = "カレントディレクトリでファイルブラウザ",
-				},
-			}
-		end)(),
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "ファイル検索" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep検索" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "バッファ一覧" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "ヘルプタグ検索" },
+			{ "<leader>sf", "<cmd>Telescope file_browser<cr>", desc = "ファイルブラウザ" },
+			-- Custom keymaps
+			{
+				";a",
+				function()
+					require("telescope.builtin").oldfiles({ initial_mode = "normal" })
+				end,
+				desc = "最近使ったファイル",
+			},
+			{
+				";b",
+				function()
+					require("telescope.builtin").current_buffer_fuzzy_find({ theme = "dropdown", sorting_strategy = "ascending" })
+				end,
+				desc = "現在バッファ内ファジー検索",
+			},
+			{
+				";f",
+				function()
+					require("telescope.builtin").find_files({ no_ignore = false, hidden = true, initial_mode = "normal" })
+				end,
+				desc = "ファイル検索(隠し含む)",
+			},
+			{
+				";r",
+				function()
+					require("telescope.builtin").live_grep()
+				end,
+				desc = "Grep検索",
+			},
+			{
+				"\\",
+				function()
+					require("telescope.builtin").buffers({ initial_mode = "normal" })
+				end,
+				desc = "バッファ一覧",
+			},
+			{
+				";t",
+				function()
+					require("telescope.builtin").help_tags({ initial_mode = "normal" })
+				end,
+				desc = "ヘルプタグ検索",
+			},
+			{
+				";;",
+				function()
+					require("telescope.builtin").resume({ initial_mode = "normal" })
+				end,
+				desc = "前回の検索を再開",
+			},
+			{
+				";e",
+				function()
+					require("telescope.builtin").diagnostics({ initial_mode = "normal" })
+				end,
+				desc = "診断一覧",
+			},
+			{
+				";c",
+				function()
+					require("telescope.builtin").git_commits({ initial_mode = "normal" })
+				end,
+				desc = "Gitコミット履歴",
+			},
+			{
+				";d",
+				function()
+					require("telescope.builtin").git_status({ initial_mode = "normal" })
+				end,
+				desc = "Gitステータス",
+			},
+			{
+				";h",
+				function()
+					require("telescope.builtin").command_history({ initial_mode = "normal" })
+				end,
+				desc = "コマンド履歴",
+			},
+			{
+				";g",
+				function()
+					require("telescope").extensions.frecency.frecency({ initial_mode = "normal" })
+				end,
+				desc = "最近使ったファイル(頻度順)",
+			},
+			{
+				"sf",
+				function()
+					local telescope = require("telescope")
+					local function telescope_buffer_dir()
+						return vim.fn.expand("%:p:h")
+					end
+					telescope.extensions.file_browser.file_browser({
+						path = "%:p:h",
+						cwd = telescope_buffer_dir(),
+						respect_gitignore = false,
+						hidden = true,
+						grouped = true,
+						previewer = false,
+						initial_mode = "normal",
+						layout_config = { height = 40 },
+					})
+				end,
+				desc = "カレントディレクトリでファイルブラウザ",
+			},
+		},
 		opts = function()
-			local actions = require("telescope.actions")
-			local fb_actions = require("telescope").extensions.file_browser.actions
-
 			return {
 				defaults = {
 					mappings = {
 						n = {
-							["q"] = actions.close,
+							["q"] = require("telescope.actions").close,
 						},
 					},
 					vimgrep_arguments = {
@@ -169,8 +163,8 @@ return {
 							},
 							["n"] = {
 								-- your custom normal mode mappings
-								["N"] = fb_actions.create,
-								["h"] = fb_actions.goto_parent_dir,
+								["N"] = require("telescope").extensions.file_browser.actions.create,
+								["h"] = require("telescope").extensions.file_browser.actions.goto_parent_dir,
 								["/"] = function()
 									vim.cmd("startinsert")
 								end,
